8e9a0b778a7773d5f885313e9578b5ab4593d3cf
Firehose <firehose@Kens-MacBook-Pro.local>
2013-11-24 14:20:08 -0500

    starting my rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Impchimp::Application.config.secret_key_base = '4d07f5f03f0c93d081d15f0f44fb5e440f643027c8540919ab2dc47f4d60124bd83def96f563678dff76300dd63c5fa003434cd253cb6224da5aa9254a47e436'
